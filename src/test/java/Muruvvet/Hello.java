d8750a8f32ec4b049069b14595a897e0706269bb
MDogan <mdoganusa@gmail.com>
2021-07-09 16:19:23 -0400

    initial commit

--- src/test/java/Muruvvet/Hello.java
+++ src/test/java/Muruvvet/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         //added more stuff
+        System.out.println("anammmm bu neydi gizzzz");
     }
 }
