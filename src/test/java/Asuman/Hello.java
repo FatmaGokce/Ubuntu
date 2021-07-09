ea34d30645cd7f2364aa4eb8a3a741e0aeef637b
asuman <ozarasuman@gmail.com>
2021-07-09 16:17:08 -0400

    first changed

--- src/test/java/Asuman/Hello.java
+++ src/test/java/Asuman/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Bismillah"); //
         System.out.println("Master");
+        System.out.println("First changed");
     }
 }
