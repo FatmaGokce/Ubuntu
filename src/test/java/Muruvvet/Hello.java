00c5fc0a701115fbfee15a0caf2f9fd7d5e19757
MDogan <mdoganusa@gmail.com>
2021-07-09 21:26:16 -0400

    last one

--- src/test/java/Muruvvet/Hello.java
+++ src/test/java/Muruvvet/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         //added more stuff
+        System.out.println("hello friends");
+
     }
 }
